dcde9bb25ce59626d04e410658edc833c63dc3fe
KhanhBui2004 <buiquockhanh005@gmail.com>
2024-11-12 15:50:19 +0700

    commit message

--- server.py
+++ server.py
@@ -2,17 +2,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-<<<<<<< HEAD
-=======
-CORS(app, resources={
-    r"/api/*": {
-        "origins": "http://localhost:3000",
-        "methods": ["GET", "POST", "PUT", "DELETE"],
-        "allow_headers": ["Content-Type", "Authorization"],
-        "supports_credentials": True
-    }
-})
->>>>>>> 1d6981ae40c2e412f5bb556a0c31a144c943f2c8
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
